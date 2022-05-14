3d232e5c0a383106780797b006b714d71d7720fe
MohamedAbdellahi98 <92870461+MohamedAbdellahi98@users.noreply.github.com>
2022-05-14 16:12:14 +0300

    Update HW_find_Path.c

--- HomeWorks/HW_find_Path.c
+++ HomeWorks/HW_find_Path.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-#define S1 6
-#define S2 12
-    int matrix[6][12] ={
+int path[6][12];
+int find_path(int a[6][12], int i, int j, int n1,int n2);
+void print_Path(int path[6][12]);
+void main(){
+	    int matrix[6][12] ={
     {1,0,1,1,1,0,0,1,0,1,1,0},
     {1,0,1,1,1,1,1,0,1,0,0,0},
     {1,1,1,1,1,1,1,0,1,0,1,0},
@@ -10,40 +12,43 @@
     {1,1,1,0,1,1,1,1,1,1,1,1}
 	};
 
-int find_path(int i, int j);
+	find_path(matrix,0,0,6,12);
+    print_Path(path);
 
-void main(){
-
-
-    int x = 6, y= 12;
-	find_path(0,0);
 }
 
-int find_path(int i, int j){
+int find_path(int a[6][12], int i, int j, int n1,int n2){
 
-    if ((i == S1-1) && (j == S2-1)){
+    if (i == n1-1 && j == n2){
+        path[i][j]=1;
         return 1;
     }
 
+   if (a[i][j] == 1){
 
-    if(i>=0 && j>=0 && i<S1&& j<S2 && matrix[i][j] == 1){
+        path[i][j]=1;
 
-        printf("(%d,%d)\n",i,j);
+        if (find_path(a,i,j+1,n1,n2) == 1)return 1;
+        if (find_path(a,i+1,j,n1,n2) == 1)return 1;
 
-        //going down
-        if(find_path(i+1, j))
-            return 1;
-        //going right
-        if(find_path(i, j+1))
-            return 1;
-        //going up
-        if(find_path(i-1, j))
-            return 1;
-        //going left
-        if(find_path(i, j-1))
-            return 1;
-
-        return 0;
+        path[i][j]=0;
     }
     return 0;
 }
+
+void print_Path(int path[6][12]){
+
+    for(int i=0;i<6;i++){
+        for(int j=0;j<12;j++)
+            if(path[i][j]==1)
+                printf("(%d,%d) -> ",i,j);
+	}
+    printf("\n");
+	for(int i=0;i<6;i++){
+        for(int j=0;j<12;j++)
+            printf("%d",path[i][j]);
+        printf("\n");
+	}
+
+}
+
